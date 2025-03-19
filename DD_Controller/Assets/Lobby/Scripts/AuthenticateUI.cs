71f088c3f055620af4970466bb573e407906dc4c
Ahmad MOULON <moulonahmad@gmail.com>
2025-03-19 17:54:06 +0100

    ✨ localize lobby

--- DD_Controller/Assets/Lobby/Scripts/AuthenticateUI.cs
+++ DD_Controller/Assets/Lobby/Scripts/AuthenticateUI.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,6 +10,7 @@ public class AuthenticateUI : MonoBehaviour {
     private void Awake() {
         authenticateButton.onClick.AddListener(() => {
             LobbyManager.Instance.Authenticate(EditPlayerName.Instance.GetPlayerName());
+            LobbyListUI.Instance.Show();
             Hide();
         });
     }
