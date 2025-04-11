d254fad01c41bdd23fa0abc46e59f2edab63d91a
Ahmad MOULON <moulonahmad@gmail.com>
2025-04-11 12:11:10 +0200

    ✨ damage dealer v0

---
+++ DD_Controller/Assets/Scripts/EnemySensorScript.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemySensorScript : MonoBehaviour
+{
+    [DoNotSerialize] public List<Collider> monstersNearby = new List<Collider>();
+    
+    private void OnTriggerEnter(Collider monster)
+    {
+        monstersNearby.Add(monster);
+    }
+    
+    private void OnTriggerExit(Collider monster)
+    {
+        monstersNearby.Remove(monster);
+    }
+}
---
+++ DD_Controller/Assets/Scripts/Move/FollowPositionManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Move
+{
+    public class FollowPositionManager : MonoBehaviour
+    {
+        public Transform[] leaders;
+        public Transform[] followers;
+        
+        void Start()
+        {
+            if (leaders.Length != followers.Length)
+            {
+                Debug.LogError("leaders and followers must have the same length");
+                return;
+            }
+        }
+        
+        void FixedUpdate()
+        {
+            for (int i = 0; i < leaders.Length; i++)
+            {
+                followers[i].position = leaders[i].position;
+            }
+        }
+    }
+}
