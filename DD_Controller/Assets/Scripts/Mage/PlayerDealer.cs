f49e520463bcef7a0f9397b9fe9587a6a70aa85d
Ahmad MOULON <moulonahmad@gmail.com>
2025-06-13 17:57:33 +0200

    ✨ Mage UI

--- DD_Controller/Assets/Scripts/Mage/PlayerDealer.cs
+++ DD_Controller/Assets/Scripts/Mage/PlayerDealer.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using System.Globalization;
 using end_game;
 using Shared;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Mage
 {
@@ -9,19 +11,52 @@ namespace Mage
     {
         public float lifePoints;
         [SerializeField] private GameEnd gameEnd;
+        
+        [SerializeField] private Slider healthBarSlider;
+        [SerializeField] private TextMeshProUGUI textHp;
+        [SerializeField] private Image damageEffect;
+
+        private string _stringMaxHp;
+        
         private void Start()
         {
             lifePoints = maxHealth;
+            _stringMaxHp = lifePoints.ToString(CultureInfo.CurrentCulture);
+            textHp.text = $"{lifePoints.ToString(CultureInfo.CurrentCulture)} / {_stringMaxHp}";
+            
+            Color damageEffectColor = damageEffect.color;
+            damageEffectColor.a = 0f;
+            damageEffect.color = damageEffectColor;
         }
 
         public void Aie(float damage)
         {
             lifePoints -= damage;
             animator?.SetTrigger(PlayerAnimP.Hurt);
+            UpdateHealthUI();
             if (lifePoints <= 0)
             {
                 gameEnd.GameOver();
             }
+        }
+
+        private void Update()
+        {
+            if (damageEffect.color.a <= 0f) return;
+            
+            Color damageEffectColor = damageEffect.color;
+            damageEffectColor.a -= Time.deltaTime;
+            damageEffect.color = damageEffectColor;
+        }
+        
+        private void UpdateHealthUI()
+        {
+            healthBarSlider.value = lifePoints / maxHealth;
+            textHp.text = $"{lifePoints.ToString(CultureInfo.CurrentCulture)} / {_stringMaxHp}";
+            
+            Color damageEffectColor = damageEffect.color;
+            damageEffectColor.a = 1f;
+            damageEffect.color = damageEffectColor;
         }
     }
 }
